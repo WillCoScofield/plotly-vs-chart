ae18a273046b623ef776c0e1fd4c0aa9a775207d
Connor <wscofield@elon.edu>
2020-08-12 21:41:02 -0400

    Minor changes to Chart.Js basic setup

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ChartsModule } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChartBarChartComponent } from './visualization/chart/chart-bar-chart/chart-bar-chart.component';
-import { ChartBubbleChartComponent } from './visualization/chart/chart-bubble-chart/chart-bubble-chart.component';
-import { ChartDoughnutChartComponent } from './visualization/chart/chart-doughnut-chart/chart-doughnut-chart.component';
-import { ChartLineChartComponent } from './visualization/chart/chart-line-chart/chart-line-chart.component';
-import { ChartPieChartComponent } from './visualization/chart/chart-pie-chart/chart-pie-chart.component';
-import { ChartRadarChartComponent } from './visualization/chart/chart-radar-chart/chart-radar-chart.component';
-import { PlotlyBarchartComponent } from './visualization/plotly/plotly-barchart/plotly-barchart.component';
+import { ChartBarChartComponent } from './visualization-packages/chart/chart-bar-chart/chart-bar-chart.component';
+import { ChartBubbleChartComponent } from './visualization-packages/chart/chart-bubble-chart/chart-bubble-chart.component';
+import { ChartDoughnutChartComponent } from './visualization-packages/chart/chart-doughnut-chart/chart-doughnut-chart.component';
+import { ChartLineChartComponent } from './visualization-packages/chart/chart-line-chart/chart-line-chart.component';
+import { ChartPieChartComponent } from './visualization-packages/chart/chart-pie-chart/chart-pie-chart.component';
+import { ChartRadarChartComponent } from './visualization-packages/chart/chart-radar-chart/chart-radar-chart.component';
+import { PlotlyBarchartComponent } from './visualization-packages/plotly/plotly-barchart/plotly-barchart.component';
 
 @NgModule({
   declarations: [
